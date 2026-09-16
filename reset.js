b4ce24024ce6565930f192f29b34a019618cd3d5
noagrp <lihwlee11@gmail.com>
2026-09-16 15:10:41 +0800

    Clean reset utility for new Firestore structure

--- reset.js
+++ reset.js
@@ -1,16 +1,15 @@
 import{initializeApp,getApps,getApp}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import{getAuth}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import{getFirestore,doc,collection,getDocs,writeBatch,updateDoc,deleteField}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import{getFirestore,doc,setDoc,collection,getDocs,writeBatch}from"https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig={apiKey:"AIzaSyAtEyvDp07Ej5KUiRaa6n0HMNrQBo6z33Y",authDomain:"boutme-3d364.firebaseapp.com",projectId:"boutme-3d364",storageBucket:"boutme-3d364.firebasestorage.app",messagingSenderId:"879605364049",appId:"1:879605364049:web:6b1b74869aaf7e00ca4f70",measurementId:"G-M4QQPYK2WT"};
 const app=getApps().length?getApp():initializeApp(firebaseConfig),auth=getAuth(app),db=getFirestore(app);
 
 async function clearCollection(uid,name){
   const snap=await getDocs(collection(db,'users',uid,name));
-  const docs=snap.docs;
-  for(let i=0;i<docs.length;i+=400){
+  for(let i=0;i<snap.docs.length;i+=400){
     const batch=writeBatch(db);
-    docs.slice(i,i+400).forEach(d=>batch.delete(d.ref));
+    snap.docs.slice(i,i+400).forEach(d=>batch.delete(d.ref));
     await batch.commit();
   }
 }
@@ -28,9 +27,8 @@ window.resetMyBoutmeProfile=async()=>{
       clearCollection(user.uid,'statuses'),
       clearCollection(user.uid,'videos')
     ]);
-    await updateDoc(doc(db,'users',user.uid),{
-      slug:'',avatar:'',name:'',shortBio:'',email:'',website:'',telephone:'',longInfo:'',address:'',
-      extraFields:deleteField(),stories:deleteField(),videos:deleteField()
+    await setDoc(doc(db,'users',user.uid),{
+      slug:'',avatar:'',name:'',shortBio:'',email:'',website:'',telephone:'',longInfo:'',address:''
     });
     alert('Profile reset complete. Your login account was kept.');
     location.reload();
